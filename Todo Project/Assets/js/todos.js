00344ba817d16abf1a9dd3d8c586ce239ded3a48
jonathan kaufman <jonathan.kaufman.18@cnu.edu>
2019-11-07 20:46:10 -0500

    Part 3 of Todo Project

--- Todo Project/Assets/js/todos.js
+++ Todo Project/Assets/js/todos.js
@@ -1,3 +1,12 @@
+//Toggle for completed Todo (Grays out and strikes through text of Todo)
 $("li").click(function () {
     $(this).toggleClass("completed");
+})
+
+//Deletes Todo
+$("span").click(function (event) {
+    $(this).parent().fadeOut(500, function () {
+        $(this).remove()
+    });
+    event.stopPropagation();
 })
